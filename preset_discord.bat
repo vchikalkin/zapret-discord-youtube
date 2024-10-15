bbb7cbafa757cd7803ce9d3e2bada5e2f5382b0f
vchikalkin <djchikalkin@gmail.com>
2024-10-15 12:03:05 +0300

    discord: add autohostlist feature

--- preset_discord.bat
+++ preset_discord.bat
@@ -10,4 +10,8 @@ set LIST=%~dp0list\
 start "zapret: discord" /min "%BIN%winws.exe" --wf-raw="@rules.txt" ^
 --filter-udp=443 --hostlist="%LIST%discord.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-udplen-increment=10 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
 --filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
---filter-tcp=443 --hostlist="%LIST%discord.txt" --dpi-desync=fake,split --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls="%BIN%tls_clienthello_www_google_com.bin"
+--filter-tcp=443 --hostlist="%LIST%discord.txt" --dpi-desync=fake,split --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls="%BIN%tls_clienthello_www_google_com.bin" --new ^
+--wf-tcp=80,443 --wf-udp=443 ^
+--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 --new ^
+--filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%LIST%autohostlist.txt" --new ^
+--dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%LIST%autohostlist.txt"
