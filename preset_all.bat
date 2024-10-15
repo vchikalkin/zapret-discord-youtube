bab868112aa0785973a9f088f5c3a0e93f8af97b
vchikalkin <djchikalkin@gmail.com>
2024-10-15 12:43:17 +0300

    add preset all

---
+++ preset_all.bat
@@ -0,0 +1,18 @@
+@echo off
+chcp 65001 >nul
+:: 65001 - UTF-8
+
+cd /d "%~dp0"
+
+set BIN=%~dp0bin\
+set LIST=%~dp0list\
+
+start "zapret: all" /min "%BIN%winws.exe" --wf-raw="@rules.txt" ^
+--filter-udp=443 --hostlist="%LIST%all.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-udplen-increment=10 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
+--filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
+--filter-tcp=80 --hostlist="%LIST%all.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443 --hostlist="%LIST%all.txt" --dpi-desync=fake,split --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls="%BIN%tls_clienthello_www_google_com.bin" --new ^
+--wf-tcp=80,443 --wf-udp=443 ^
+--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 --new ^
+--filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%LIST%autohostlist.txt" --new ^
+--dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%LIST%autohostlist.txt"
